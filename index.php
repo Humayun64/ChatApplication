dbe37368a4834bd88e2ec81588b62782e620b184
Humayun Kabir <you@example.com>
2024-01-19 11:54:49 +0600

    put some important

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Chat Applicaton</title>
+</head>
+<body>
+<h1>Chat Application</h1>
+
+</body>
+</html>
